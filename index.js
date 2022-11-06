934ffd7954a32a933769d50e0ac89e6cff2b5c46
d3v-tushar <d3v.tushar@gmail.com>
2022-11-07 02:24:12 +0600

    CRUD With MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,19 +13,58 @@ const uri = "mongodb+srv://dbuser01:tushar2151@learnph.159fxoq.mongodb.net/?retr
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const run = async() =>{
     try{
-    const userCollection = client.db('nodeMongoCrud').collection('users');
-    app.get('/users', async(req, res) =>{
-        const query = {};
-        const cursor = userCollection.find(query);
-        const users = await cursor.toArray();
-        res.send(users);
-    })
-    app.post('/users', async(req, res) =>{
-        const user = req.body;
-        console.log(user);
-        const result = await userCollection.insertOne(user);
-        res.send(result);
-    })
+        const userCollection = client.db('nodeMongoCrud').collection('users');
+
+        //Create
+        app.post('/users', async(req, res) =>{
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+        //READ
+        app.get('/users', async(req, res) =>{
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        });
+
+        //READ - (Dynamic)
+        app.get('/users/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await userCollection.findOne(query);
+            res.send(result);
+        });
+
+        //Update
+        app.put('/users/:id', async(req, res) =>{
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true };
+            const user = req.body;
+            console.log(user);
+            const updateUser = {$set: {
+                name: user.name,
+                address: user.address,
+                email: user.email
+            }}
+            const result = await userCollection.updateOne(filter, updateUser, options);
+            res.send(result);
+
+        });
+
+        //Delete
+        app.delete('/users/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            console.log(result);
+            res.send(result);
+        })
+
     }
     finally{
 
