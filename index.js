e58c503ff33437e257cea7d2f7095bb652803407
d3v-tushar <d3v.tushar@gmail.com>
2022-11-06 20:20:24 +0600

    Create & Read Operation Complete

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = process.nextTick.PORt || 5000;
+const port = process.env.PORT || 5000;
 
 //Middleware
 app.use(cors());
 app.use(express.json());
+
+//MongoDB CRUD Server
+const uri = "mongodb+srv://dbuser01:tushar2151@learnph.159fxoq.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async() =>{
+    try{
+    const userCollection = client.db('nodeMongoCrud').collection('users');
+    app.get('/users', async(req, res) =>{
+        const query = {};
+        const cursor = userCollection.find(query);
+        const users = await cursor.toArray();
+        res.send(users);
+    })
+    app.post('/users', async(req, res) =>{
+        const user = req.body;
+        console.log(user);
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+    })
+    }
+    finally{
+
+    }
+}
+run().catch(error => console.log(error));
 
 app.get('/', (req, res) =>{
     res.send('Hello From Node MongoDB CRUD Sever');
